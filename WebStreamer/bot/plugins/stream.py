6aecbe52c0875fdb117a83817df2df70c83d6d48
adarshgoelurl <88939380+adarshgoelurl@users.noreply.github.com>
2021-09-30 21:46:01 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -6,6 +6,16 @@ from WebStreamer.vars import Var
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
+def detect_type(m: Message):
+    if m.document:
+        return m.document
+    elif m.video:
+        return m.video
+    elif m.audio:
+        return m.audio
+    else:
+        return
+
 
 @StreamBot.on_message(filters.private & (filters.document | filters.video | filters.audio), group=4)
 async def media_receive_handler(_, m: Message):
@@ -13,7 +23,7 @@ async def media_receive_handler(_, m: Message):
     stream_link = Var.URL + 'stream/' + str(log_msg.message_id)
     online_link = Var.URL + str(log_msg.message_id)
     await m.reply_text(
-        text="`{}`".format(stream_link),
+        text="`{}`".format(stream_link), "`{}`".format(stream_link)
         quote=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Stream', url=stream_link),
                                             InlineKeyboardButton('Download', url=online_link)]])

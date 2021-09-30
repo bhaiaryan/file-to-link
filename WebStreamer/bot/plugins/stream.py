2aefd14ae97530bf3ffbadd482d67292d1dac652
adarshgoelurl <88939380+adarshgoelurl@users.noreply.github.com>
2021-09-30 22:50:49 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -15,6 +15,7 @@ def detect_type(m: Message):
         return m.audio
     else:
         return
+krt ="      "
 
 
 @StreamBot.on_message(filters.private & (filters.document | filters.video | filters.audio), group=4)
@@ -23,7 +24,7 @@ async def media_receive_handler(_, m: Message):
     stream_link = Var.URL + 'stream/' + str(log_msg.message_id)
     online_link = Var.URL + str(log_msg.message_id)
     await m.reply_text(
-        text="`{}`".format(stream_link "_________" + "______" online_link),
+        text="`{}`".format(stream_link  + krty + online_link),
         quote=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Stream', url=stream_link),
                                             InlineKeyboardButton('Download', url=online_link)]])
